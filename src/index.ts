7fad667ed2278232a16eb8e683526cb736533ea6
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-22 12:05:03 +0800

    提交代码

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import { ethers } from 'ethers';
+import ERC20_ABI from '@openzeppelin/contracts/build/contracts/ERC20.json';
+
+
 
 const secret = require('../.secret.json');
 
@@ -9,6 +12,13 @@ async function main() {
   console.log(wallet.address);
   const balance = await provider.getBalance(wallet.address);
   console.log(balance);
+  const uniToken = new ethers.Contract(
+    '0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984',
+    ERC20_ABI.abi,
+    provider,
+  );
+  const a = await uniToken.symbol();
+  console.log(a);
 }
 
 main();
