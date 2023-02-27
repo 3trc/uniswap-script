9810767d0f8e6992357098887c6d497809588aa8
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-27 14:51:55 +0800

    提交代码

--- src/a.ts
+++ src/a.ts
@@ -6,7 +6,7 @@ import { Wallet } from './lib/runner';
 
 async function main() {
   const token = await CreateERC20('0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 5, Wallet);
-  const t = await token.approve('0x4648a43B2C14Da09FdF82B161150d3F634f40491', ethers.parseEther('0'));
+  const t = await token.approve('0xE592427A0AEce92De3Edee1F18E0157C05861564', ethers.parseEther('0'));
   console.log(t);
   await t.wait();
   console.log('done');
