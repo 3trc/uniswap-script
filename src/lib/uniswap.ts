4da43b5c475245571e1ed9b5a6e523e52b43ef0f
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-28 18:23:59 +0800

    getOutputQuote

--- src/lib/uniswap.ts
+++ src/lib/uniswap.ts
@@ -1,12 +1,15 @@
+import { Currency, CurrencyAmount } from '@uniswap/sdk-core';
 import IUniswapV3PoolABI from '@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json';
-import { computePoolAddress, Pool, Route } from '@uniswap/v3-sdk';
+import { computePoolAddress, Pool, Route, SwapQuoter } from '@uniswap/v3-sdk';
 import { ethers } from 'ethers';
 import { ERC20 } from './erc20';
-import { Wallet } from './runner';
+import { Provider, Wallet } from './runner';
 
 const POOL_FACTORY_CONTRACT_ADDRESS = '0x1F98431c8aD98523631AE4a59f267346ea31F984';
 
 const POOL_FEE = 500;
+
+const QUOTER_CONTRACT_ADDRESS = '0x61fFE014bA17989E743c5F6cB21bF9697530B21e';
 
 export
 function createPoolContract(tokenIn: ERC20, tokenOut: ERC20) {
@@ -49,4 +52,25 @@ function createRoute(
   tokenOut: ERC20,
 ) {
   return new Route([pool], tokenIn.Token, tokenOut.Token);
+}
+
+export
+async function getOutputQuote(
+  tokenIn: ERC20,
+  amount: string,
+  route: Route<Currency, Currency>,
+) {
+  const { calldata } = await SwapQuoter.quoteCallParameters(
+    route,
+    CurrencyAmount.fromRawAmount(tokenIn.Token, tokenIn.Parse(amount).toString()),
+    0,
+    {
+      useQuoterV2: true,
+    },
+  );
+  const quoteCallReturnData = await Provider.call({
+    to: QUOTER_CONTRACT_ADDRESS,
+    data: calldata,
+  });
+  return new ethers.AbiCoder().decode(['uint256'], quoteCallReturnData);
 }
