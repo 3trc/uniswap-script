1ede7b4c9b625e1bac6c03c3040573b1bdc93c66
jimao <jimao@jimaodeMacBook-Pro.local>
2023-02-28 17:38:05 +0800

    找到了执行者的地址

--- src/a.ts
+++ src/a.ts
@@ -6,7 +6,7 @@ import { Wallet } from './lib/runner';
 
 async function main() {
   const token = await CreateERC20('0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 5, Wallet);
-  const t = await token.approve('0xE592427A0AEce92De3Edee1F18E0157C05861564', ethers.parseEther('0'));
+  const t = await token.approve('0x000000000022d473030f116ddee9f6b43ac78ba3', ethers.parseEther('0'));
   console.log(t);
   await t.wait();
   console.log('done');
