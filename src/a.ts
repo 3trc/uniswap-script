0ad8c34a9d8b84ce283d7ff760fb89b28f5375ed
jimao <jimao@jimaodeMBP.lan>
2023-03-06 20:51:02 +0800

    提交代码

--- src/a.ts
+++ src/a.ts
@@ -3,7 +3,14 @@ import { createERC20, ERC20 } from './lib/erc20';
 import { Wallet } from './lib/runner';
 import { createPoolContract, createPool, createRoute, getOutputQuote, createTrade, createTradeTransaction } from './lib/uniswap';
 
-async function swap(tokenIn: ERC20, amountIn: bigint, tokenOut: ERC20) {
+async function trade(
+  assets: ERC20,
+  funds: ERC20,
+  amountIn: bigint,
+  side: 'buy' | 'sell',
+) {
+  const tokenIn = side === 'buy' ? funds : assets;
+  const tokenOut = side === 'buy' ? assets : funds;
   const poolContract = createPoolContract(tokenIn, tokenOut);
   const pool = await createPool(poolContract, tokenIn, tokenOut);
   const route = createRoute(pool, tokenIn, tokenOut);
@@ -23,14 +30,13 @@ async function swap(tokenIn: ERC20, amountIn: bigint, tokenOut: ERC20) {
   console.log('结束');
 }
 
-
 async function main() {
-  const [tokenIn, tokenOut] = await Promise.all([
+  const [WETH, UNI] = await Promise.all([
     createERC20('0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6', 5, Wallet),
     createERC20('0x1f9840a85d5af5bf1d1762f925bdaddc4201f984', 5, Wallet),
   ]);
-  const tokenInBalance = await tokenIn.balanceOf(Wallet.address);
-  await swap(tokenIn, tokenInBalance, tokenOut);
+  const tokenInBalance = await WETH.balanceOf(Wallet.address);
+  trade(UNI, WETH, tokenInBalance, 'buy');
 }
 
 main();
