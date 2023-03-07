a0e2dd4a29ec575ab6e6dafced2361fec2ffa254
jimao <jimao@jimaodeMacBook-Pro.local>
2023-03-07 17:59:18 +0800

    提交代码

--- src/arb.ts
+++ src/arb.ts
@@ -1,7 +1,16 @@
+import { createERC20 } from './lib/erc20';
+import { Wallet, Provider } from './lib/runner';
 
 
 async function main() {
-  console.log('你好，世界');
+  const [USDC, WETH] = await Promise.all([
+    createERC20('0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8', 42161, Wallet),
+    createERC20('0x82aF49447D8a07e3bd95BD0d56f35241523fBab1', 42161, Wallet),
+  ]);
+  const balance = await WETH.balanceOf(Wallet.address);
+  console.log(WETH.Format(balance));
+  const balance2 = await USDC.balanceOf(Wallet.address);
+  console.log(USDC.Format(balance2));
 }
 
 main();
