6a086c8daf80ba20450875a852ce8e4979a14fac
jimao <jimao@jimaodeMacBook-Pro.local>
2023-03-07 15:42:06 +0800

    提交代码

--- src/lib/erc20.ts
+++ src/lib/erc20.ts
@@ -1,31 +1,38 @@
 import { Token } from '@uniswap/sdk-core';
 import ERC20_ABI from '@openzeppelin/contracts/build/contracts/ERC20.json';
 import { ethers } from 'ethers';
-import { ContractRunner } from 'ethers/types/providers';
 
 export
-async function createERC20(address: string, chainId: number, runner: ContractRunner) {
-  const contract = new ethers.Contract(address, ERC20_ABI.abi, runner);
+async function createERC20(
+  address: string,
+  chainId: number,
+  wallet: ethers.Wallet,
+) {
+  const contract = new ethers.Contract(address, ERC20_ABI.abi, wallet);
   const [symbol, name, decimals]: [string, string, bigint] = await Promise.all([
     contract.symbol(),
     contract.name(),
     contract.decimals(),
   ]);
-  return new ERC20(runner, chainId, address, Number(decimals), symbol, name);
+  return new ERC20(wallet, chainId, address, Number(decimals), symbol, name);
 }
 
 export
 class ERC20 {
   public constructor(
-    private runner: ContractRunner,
+    private wallet: ethers.Wallet,
     ...args: ConstructorParameters<typeof Token>
   ) {
     this.token = new Token(...args);
-    this.contract = new ethers.Contract(this.token.address, ERC20_ABI.abi, this.runner);
+    this.contract = new ethers.Contract(this.token.address, ERC20_ABI.abi, this.wallet);
   }
 
   private token: Token;
   private contract: ethers.Contract;
+
+  public get Wallet() {
+    return this.wallet;
+  }
 
   public get Token() {
     return this.token;
